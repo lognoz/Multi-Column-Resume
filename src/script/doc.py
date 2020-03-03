dd2c42faf757cb284640b6d0109ed282649aab3b
Marc-Antoine Loignon <developer@lognoz.org>
2020-03-02 19:53:53 -0500

    Add python script to change mockup into documentation

--- src/script/doc.py
+++ src/script/doc.py
@@ -5,9 +5,19 @@ Copyright (c) Marc-Antoine Loignon
 See the file 'LICENSE' for copying permission
 """
 
+import fileinput
+from time import time
 from PIL import Image, ImageOps
 from pdf2image import convert_from_path
 
+
+def edit_file(filename):
+    """
+    This function is used to edit file.
+    """
+    with fileinput.input(filename, inplace=1) as f:
+        for line in f:
+            yield line.rstrip('\n')
 
 def convert_pdf_to_png():
     """
@@ -18,7 +28,7 @@ def convert_pdf_to_png():
     for page in pages:
         page.save("output/resume.png", "PNG")
 
-def create_mockup(image, background):
+def create_mockup(image, background, timestamp):
     """
     This function create a mockup image.
     """
@@ -31,7 +41,17 @@ def create_mockup(image, background):
 
     target.paste(background)
     target.paste(image, (margin, margin))
-    target.save("output/resume.png", quality=100)
+    target.save("output/resume." + str(timestamp) + ".png", quality=100)
+
+def change_documentation(timestamp):
+    """
+    This function is used to change mockup in documentation.
+    """
+    for line in edit_file("README.md"):
+        if line.startswith("![Curriculum vitae example]"):
+            print("![Curriculum vitae example](./output/resume." + str(timestamp) + ".png)")
+        else:
+            print(line)
 
 def main():
     """
@@ -40,7 +60,9 @@ def main():
     convert_pdf_to_png()
     image = Image.open("output/resume.png")
     background = Image.open("asset/background.jpg")
-    create_mockup(image, background)
+    timestamp = time()
+    create_mockup(image, background, timestamp)
+    change_documentation(timestamp)
 
 if __name__ == '__main__':
     main()
